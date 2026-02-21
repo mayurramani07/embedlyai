023d787a6be54ddddc62f320bd3595d4e941e780
Mayur Ramani <ramanimayur836@gmail.com>
2026-02-22 00:34:16 +0530

    feat(home): Add animated fixed navbar using Motion

--- src/components/HomeClient.tsx
+++ src/components/HomeClient.tsx
@@ -1,8 +1,21 @@
+'use client'
 import React from 'react'
-
+import {motion} from "motion/react"
 function HomeClient() {
   return (
-    <div>Hello</div>
+    <div className='min-h-screen bg-linear-to-br from-white to-zinc-50 text-zinc-900 overflow-x-hidden'>
+        <motion.div 
+        initial = {{y:-60}}
+        animate = {{y:0}}
+        transition={{duration:0.7}}
+        className='fixed top-0 left-0 w-full z-50 bg-white/70 backdrop-blur-xl border-b border-zinc-200'>
+        <div className='max-w-7xl mx-auto px-6 h-16 flex items-center justify-between'>
+            <div className='text-lg font-semibold tracking-tight'>embedly<span className='text-zinc-400'>AI</span></div>
+            <motion.button className='px-5 py-2 rounded-full bg-black text-white text-sm font-medium hover:bg-zinc-800 transition disabled:opacity-60 flex items-baseline-last gap-2'>Login</motion.button>
+        </div>
+
+        </motion.div>
+    </div>
   )
 }
 
