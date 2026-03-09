4d801d343aa57589edbbf1541d65aded52980839
Mayur Ramani <ramanimayur836@gmail.com>
2026-03-09 22:48:18 +0530

    feat(build): implement user storage Mongoose schema and model

---
+++ src/models/user.storage.ts
@@ -0,0 +1,34 @@
+import mongoose, { model, Schema } from "mongoose";
+
+interface IUserStorage {
+  ownerId: string;
+  businessName: string;
+  supportEmail: string;
+  knowledge: string;
+}
+
+const userStorageSchema = new Schema<IUserStorage>(
+  {
+    ownerId: {
+      type: String,
+      required: true,
+    },
+    businessName: {
+      type: String,
+      required: true,
+    },
+    supportEmail: {
+      type: String,
+      required: true,
+    },
+    knowledge: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const userStorage = mongoose.models.Setting || model("Setting", userStorageSchema);
+
+export default userStorage;
